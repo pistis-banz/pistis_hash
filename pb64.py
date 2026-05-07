e223ca2c8e9d8c2ee991ac76e654ba4ff37cfc62
pistisbanz <pistisbasolua@gmail.com>
2026-05-07 23:14:29 +0100

    initialisation du projet

---
+++ pb64.py
@@ -0,0 +1,26 @@
+class PB64:
+    def init(self):
+        self.h = 0x5042535449533634
+
+    def rotate_left(self, x, n):
+        return ((x << n) | (x >> (64 - n))) & 0xFFFFFFFFFFFFFFFF
+
+    def hash(self, message):
+        h = self.h
+
+        data = message.encode("utf-8")
+
+        for i, byte in enumerate(data):
+            h ^= byte
+            h = self.rotate_left(h, (i % 13) + 1)
+            h = (h * 0x9E3779B185EBCA87) & 0xFFFFFFFFFFFFFFFF
+            h ^= 0x5042
+
+        return f"{h:016x}"
+
+
+algo = PB64()
+
+texte = input("Entrer un texte : ")
+
+print("Hash PB64 :", algo.hash(texte))
